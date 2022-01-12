ed545377beffe4896be2386c4519c11051d4dc68
Jakob Bouchard <24767889+jakobbouchard@users.noreply.github.com>
2022-01-12 17:50:32 -0500

    Konami code for rainbowww

--- main.js
+++ main.js
@@ -1,3 +1,27 @@
 (function() {
+	const pattern = ['ArrowUp', 'ArrowUp', 'ArrowDown', 'ArrowDown', 'ArrowLeft', 'ArrowRight', 'ArrowLeft', 'ArrowRight', 'b', 'a', 'Enter'];
+	let current = 0;
 
+	const keyHandler = function (event) {
+
+		// If the key isn't in the pattern, or isn't the current key in the pattern, reset
+		if (pattern.indexOf(event.key) < 0 || event.key !== pattern[current]) {
+			current = 0;
+			return;
+		}
+
+		// Update how much of the pattern is complete
+		current++;
+
+		// If complete, alert and reset
+		if (pattern.length === current) {
+			current = 0;
+			document.body.style.animation = '10s linear 0s infinite rainbow-text';
+			document.querySelector('header').style.animation = '10s linear 0s infinite rainbow-background';
+		}
+
+	};
+
+	// Listen for keydown events
+	document.addEventListener('keydown', keyHandler, false);
 })();
