ee4c54c4cb399abed38cacdf3437385e348b6c47
Jakob Bouchard <24767889+jakobbouchard@users.noreply.github.com>
2022-02-26 13:33:23 -0500

    Add download link, with password

--- scripts/main.js
+++ scripts/main.js
@@ -3,7 +3,6 @@
 	let current = 0;
 
 	const keyHandler = function (event) {
-
 		// If the key isn't in the pattern, or isn't the current key in the pattern, reset
 		if (pattern.indexOf(event.key) < 0 || event.key !== pattern[current]) {
 			current = 0;
@@ -23,4 +22,26 @@
 
 	// Listen for keydown events
 	document.addEventListener('keydown', keyHandler, false);
+
+	function selectText(node) {
+		if (document.body.createTextRange) {
+			const range = document.body.createTextRange();
+			range.moveToElementText(node);
+			range.select();
+		} else if (window.getSelection) {
+			const selection = window.getSelection();
+			const range = document.createRange();
+			range.selectNodeContents(node);
+			selection.removeAllRanges();
+			selection.addRange(range);
+		} else {
+			console.warn("Could not select text in node: Unsupported browser.");
+		}
+	}
+	document.querySelectorAll('code').forEach((el) => {
+		el.addEventListener('click', () => {
+			// Select the text
+			selectText(el);
+		});
+	});
 })();
